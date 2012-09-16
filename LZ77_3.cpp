cac937a3ba7fc45b44def8fb1948214056f67720
jshun <jshun@cs.cmu.edu>
2012-09-16 18:06:32 -0400

    a

--- LZ77_3.cpp
+++ LZ77_3.cpp
@@ -12,126 +12,87 @@
 #include "suffixArray.h"
 using namespace std;
 
-void sop(int i, int l, int j, int* LPS, int* PrevOcc){
-  if(LPS[i] == -1){
-    LPS[i] = l;
-    PrevOcc[i] = j;
-  } else {
-    if(LPS[i] < l){
-      if(PrevOcc[i] > j)
-	sop(PrevOcc[i],LPS[i],j,LPS,PrevOcc);
+void sop(int i, int l, int j, int *lps, int *prev_occ, int bot){
+  if( j == 0 and l ==0 and i==0 )
+    return;
+  // assert(i>j);
+  // assert(i!=j);
+  if( lps[i] == bot ){
+    lps[i] = l;
+    prev_occ[i] = j;
+  }else{
+    if( lps[i] < l ){
+      if( prev_occ[i] > j )
+	sop(prev_occ[i], lps[i], j, lps, prev_occ, bot);
       else
-	sop(j,LPS[i],PrevOcc[i],LPS,PrevOcc);
-      LPS[i] = l;
-      PrevOcc[i] = j;
-    } else {
-      if(PrevOcc[i] > j)
-	sop(PrevOcc[i],l,j,LPS,PrevOcc);
+	sop(j, lps[i],prev_occ[i], lps, prev_occ, bot);
+      lps[i] = l;
+      prev_occ[i] = j;
+    }else{
+      if( prev_occ[i] > j )
+	sop(prev_occ[i], l, j, lps, prev_occ, bot);
       else
-	sop(j,l,PrevOcc[i],LPS,PrevOcc);
-    } 
+	sop(j, l, prev_occ[i],lps, prev_occ, bot);			
+    }
   }
 }
 
 pair<int*,int> LempelZiv(int *s, int n) {
-    timer lzTm;
-    lzTm.start();
+  timer lzTm;
+  lzTm.start();
 
-    pair<int *, int *> res = suffixArray(s, n, 0);
+  pair<int *, int *> res = suffixArray(s, n, 0);
 
-    int *SA = res.first;
-    lzTm.reportNext("\tsuffix array");
+  int *sa = res.first;
+  lzTm.reportNext("\tsuffix array");
 
-    //for(int i=0;i<n;i++) cout<<SA[i]<<" ";cout<<endl;
-    
-    // //begin bullshit
-    // int* LPS = new int[n];
-    // int* PrevOcc = new int[n];
-    // for(int i=0;i<n;i++) LPS[i] = PrevOcc[i] = -1;
-    // int* Phi = new int[n];
+  //for(int i=0;i<n;i++) cout<<SA[i]<<" ";cout<<endl;
+  
+  int *phi = new int [n];
+  int *prev_occ = new int [n];
+  for(int i=0; i<n; ++i)
+    prev_occ[i] = s[i];
+  int to_add[2] = {-1,n-1};
+  for(int i=0; i<n; ++i){
+    phi[sa[i]] = sa[i+to_add[i==0]];
+  }
+  
+  // sa holds now LPS
+  for(int i=0; i<n; ++i)
+    sa[i] = -1;	   
 
-    // Phi[SA[0]] = 0;
-    // for(int i=1;i<n;i++) Phi[SA[i]] = SA[i-1];
-    // int l = 0;
-    // for(int i=0;i<n;i++){
-    //   int j = Phi[i];
-    //   while(s[i+l] == s[j+l]) l++;
-    //   if(i >= j) sop(i,l,j,LPS,PrevOcc);
-    //   else sop(j,l,i,LPS,PrevOcc);
-    //   l = max(l-1,0);
-    // }
-    // //end bullshit
-    // for(int i=0;i<n;i++) cout<<LPS[SA[i]]<<" ";cout<<endl;
-    // for(int i=0;i<n;i++) cout<<PrevOcc[SA[i]]<<" ";cout<<endl;
-    
-    int san = n+1;
-    int *phi = new int [san];
-    int *prev_occ = new int [san];
-    //for(int i=0; i<n; ++i)
-    //prev_occ[i] = s[i];
-    int to_add[2] = {-1,san-1};
-    phi[SA[0]] = n;
-
-    for(int i=1; i<n; ++i){      
-      phi[SA[i]] = SA[i-1];
+  int l = 0;
+  for(int i=0; i<n; ++i){
+    int j = phi[i];
+    while( s[i+l] == s[j+l] ) ++l;
+    if( i>j ){
+      sop(i,l,j,sa,prev_occ,-1);
+    }else{
+      sop(j,l,i,sa,prev_occ,-1);
     }
-    phi[n] = SA[n-1];
-    //for(int i=0;i<san;i++)cout<<phi[i]<<" ";cout<<endl;
-    int *LPS = new int[san];
-    // sa holds now LPS
-    for(int i=0; i<san; ++i)
-      LPS[i] = n;	   
+    if( l > 0 ) --l;	 
+  }
 
-    int l = 0;
-    for(int i=0; i<n; ++i){
- 
-      int j = phi[i];
-      while( s[i+l] == s[j+l] ) ++l;
-      int ii = 0, jj = 0;
-      if( i > j){
-	ii = i; jj = j;
-      }else{
-	ii = j; jj = i;
-      }
-      int ll = l;
-      while( LPS[ii] != n ){
-	int iii = (prev_occ[ii] > jj) ? prev_occ[ii] : jj;
-	int jjj = (prev_occ[ii] > jj) ? jj : prev_occ[ii];
-	if( LPS[ii] < ll ){
-	  int lll = LPS[ii];
-	  LPS[ii] = lll;
-	  prev_occ[ii] = jj;
-	  ll = lll;
-	}
-	ii = iii;
-	jj = jjj;
-      }
-      LPS[ii] = ll; prev_occ[ii] = jj;
-      if( l > 0 ) --l;	 
-    }
+  // for(int i=0;i<n;i++)cout<<sa[i]<<" ";cout<<endl;
+  // for(int i=0;i<n;i++)cout<<prev_occ[i]<<" ";cout<<endl;
 
-    //for(int i=0;i<n;i++) cout<<LPS[i]<<" ";cout<<endl;
-    //for(int i=0;i<n;i++) cout<<prev_occ[i]<<" ";cout<<endl;
+  lzTm.reportNext("\tLPF");
 
-    lzTm.reportNext("\tLPF");
+  delete prev_occ; delete phi;
 
-
-    delete prev_occ; delete phi;
-    free(SA);
-
-    //compute LZ array
-    int* LZ = newA(int,n);
-    LZ[0] = 0;
-    int j = 0;
-    while(LZ[j] < n){
-      LZ[j+1] = LZ[j] + max(1,LPS[LZ[j]]);
-      j++;
-    }
-    lzTm.reportNext("\tLZ");
-    delete LPS;
-    return make_pair(LZ, j);
+  //compute LZ array
+  int* LZ = newA(int,n);
+  LZ[0] = 0;
+  int j = 0;
+  while(LZ[j] < n){
+    LZ[j+1] = LZ[j] + max(1,sa[LZ[j]]);
+    j++;
+  }
+  lzTm.reportNext("\tLZ");
+  free(sa);
+  return make_pair(LZ, j);
 }
 
 int main(int argc, char *argv[]) {
-    return test_main(argc, argv, (char *)"Seq LZ77 CPM2011", LempelZiv);
+  return test_main(argc, argv, (char *)"Seq LZ77 CPM2011", LempelZiv);
 }
